87d6f74827821e5ed948535f155640c8c9896c81
백승주 <137677440+bearkuang@users.noreply.github.com>
2024-05-06 15:59:15 +0900

    [Modify] delete duplicated directory

--- SocialLogin/src/main/java/com/social/test/handler/ex/CustomApiException.java
+++
@@ -1,8 +0,0 @@
-package com.social.test.handler.ex;
-
-public class CustomApiException extends RuntimeException {
-
-    public CustomApiException(String message) {
-        super(message);
-    }
-}
